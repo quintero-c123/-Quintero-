80a36b747b754521134ae14cfe4e31b012d474ba
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2021-04-21 22:17:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089'] // Put your number here
+global.owner = ['573104020996'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -13,7 +13,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'JEIRONY'
 
 global.multiplier = 69 // The higher, The harder levelup
 
